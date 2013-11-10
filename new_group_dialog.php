50b6edb3c6933722e711c6de09ea0c2302694b67
techird <techird@qq.com>
2013-11-10 11:25:34 +0800

    1110

---
+++ new_group_dialog.php
@@ -0,0 +1,21 @@
+<div id="new-group-dialog" class="dialog-widget">
+    <div class="dialog-title"><h2>Create New Group</h2></div>
+    <div class="dialog-content">
+        <form class="stand-alone longer">
+            <div class="field">
+                <label>Group Name<span class="required">*</span></label>
+                <select>
+                    <option>Group 1</option>
+                    <option>Group 2</option>
+                    <option>Group 3</option>
+                </select>
+            </div>
+            <div class="padding-box"></div>
+            <div class="field">
+                <label></label>
+                <button id="save-offer-btn" class="submit small">Save</button>
+                <a class="close-dialog">Cancel</a>
+            </div>
+        </form>
+    </div>
+</div>
